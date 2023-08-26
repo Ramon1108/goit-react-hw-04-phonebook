6734d617558f9de65870e930c8eb019e2c0626da
Ramon1108 <122237742+Ramon1108@users.noreply.github.com>
2023-08-26 20:58:30 +0300

    1

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,70 @@
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import css from './ContactForm.module.css';
+
+class ContactForm extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = e => {
+    const { name, value } = e.currentTarget;
+
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const { name, number } = this.state;
+
+    if (name.trim() === '' || number.trim() === '') {
+      alert("Enter the contact's name and number phone!");
+    } else if (!/\d{3}[-]\d{2}[-]\d{2}/g.test(number)) {
+      alert('Enter the correct number phone!');
+    } else {
+      this.props.onSubmit({ name, number });
+      this.setState({ name: '', number: '' });
+    }
+  };
+
+  render() {
+    const { name, number } = this.state;
+    return (
+      <form className={css.form} onSubmit={this.handleSubmit}>
+        <label className={css.label}>
+          Name
+          <input
+            className={css.input}
+            type="text"
+            name="name"
+            value={name}
+            onChange={this.handleChange}
+            placeholder="Ivan Ivanov"
+          />
+        </label>
+        <label className={css.label}>
+          Number
+          <input
+            className={css.input}
+            type="text"
+            name="number"
+            value={number}
+            onChange={this.handleChange}
+            placeholder="111-11-11"
+          />
+        </label>
+        <button className={css.btn} type="submit">
+          Add contact
+        </button>
+      </form>
+    );
+  }
+}
+
+ContactForm.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+export default ContactForm;
